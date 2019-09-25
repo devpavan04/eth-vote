7ec455730e95242d46fa9fb3d5bed1bce78671e9
devPavan04 <pavanr.cs16@sahyadri.edu.in>
2019-09-25 15:12:38 +0530

    very close to working

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -8,11 +8,16 @@ contract Election {
         string name;
         uint voteCount;
     }
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
     // Store candidates
     mapping(uint => Candidate) public candidates;
-    // Fetch candidate
     // Store candidates count
     uint public candidatesCount; // candidatesCount is now 0
+
+    event votedEvent (
+        uint indexed _candidateId
+    );
 
     constructor ( ) public {
         addCandidate("Jimmy Fallon");
@@ -22,5 +27,18 @@ contract Election {
     function addCandidate(string memory _name) private {
         candidatesCount ++; // it will be incremented from 0 to 1
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote(uint _candidateId) public {
+        // Require that they haven't voted before
+        require(!voters[msg.sender], "you have already casted your vote");
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount, "candidate is not valid");
+        // Record the voter has voted
+        voters[msg.sender] = true;
+        // Update candidate vote count
+        candidates[_candidateId].voteCount ++;
+        // Trigger voted event
+        emit votedEvent(_candidateId);
     }
 }
--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@ App = {
 	web3Provider: null,
 	contracts: {},
 	account: '0x0',
-	// hasVoted: false,
+	hasVoted: false,
 
 	init: function () {
 		return App.initWeb3();
@@ -28,31 +28,34 @@ App = {
 			App.contracts.Election = TruffleContract(election);
 			// Connect provider to interact with contract
 			App.contracts.Election.setProvider(App.web3Provider);
-
-			// App.listenForEvents();
-
+			App.listenForEvents();
 			return App.render();
 		});
 	},
 
 	// Listen for events emitted from the contract
-	// listenForEvents: function () {
-	// 	App.contracts.Election.deployed().then(function (instance) {
-	// 		// Restart Chrome if you are unable to receive this event
-	// 		// This is a known issue with Metamask
-	// 		// https://github.com/MetaMask/metamask-extension/issues/2393
-	// 		instance.votedEvent({}, {
-	// 			fromBlock: 0,
-	// 			toBlock: 'latest'
-	// 		}).watch(function (error, event) {
-	// 			console.log("event triggered", event)
-	// 			// Reload when a new vote is recorded
-	// 			App.render();
-	// 		});
-	// 	});
-	// },
+	listenForEvents: function () {
+		App.contracts.Election.deployed().then(function (instance) {
+			// Restart Chrome if you are unable to receive this event
+			// This is a known issue with Metamask
+			// https://github.com/MetaMask/metamask-extension/issues/2393
+			instance.votedEvent({}, {
+				fromBlock: 0,
+				toBlock: 'latest'
+			}).watch(function (error, event) {
+				console.log("event triggered", event)
+				// Reload when a new vote is recorded
+				App.render();
+			});
+		});
+	},
 
-	render: function ()  {
+
+	// In the render() function,
+	// 1st we list out all the candidates standing for the election in the form select
+	// 2nd we will handle the form submission whenever the account tries to vote
+	// 3rd we want to hide the form once the account vote
+	render: function () {
 		var electionInstance;
 		var loader = $("#loader");
 		var content = $("#content");
@@ -64,7 +67,7 @@ App = {
 		web3.eth.getAccounts(function (err, account) {
 			if (err === null) {
 				App.account = account;
-				$("#accountAddress").html("Your Account: " + account);
+				$("#accountAddress").html("Your Account: " + App.account);
 			}
 		});
 
@@ -76,8 +79,8 @@ App = {
 			var candidatesResults = $("#candidatesResults");
 			candidatesResults.empty();
 
-			// var candidatesSelect = $('#candidatesSelect');
-			// candidatesSelect.empty();
+			var candidatesSelect = $('#candidatesSelect');
+			candidatesSelect.empty();
 
 			for (var i = 1; i <= candidatesCount; i++) {
 				electionInstance.candidates(i).then(function (candidate) {
@@ -86,20 +89,20 @@ App = {
 					var voteCount = candidate[2];
 
 					// Render candidate Result
-					var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td></tr>"
+					var candidateTemplate =  "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td></tr>"
 					candidatesResults.append(candidateTemplate);
 
-					// Render candidate ballot option
-					// var candidateOption = "<option value='" + id + "' >" + name + "</ option>"
-					// candidatesSelect.append(candidateOption);
+					// Render candidate select option
+					var candidateOption = "<option value='" + id + "' >" + name + "</ option>"
+					candidatesSelect.append(candidateOption);
 				});
 			}
-		// 	return electionInstance.voters(App.account);
-		// }).then(function (hasVoted) {
-		// 	// Do not allow a user to vote
-		// 	if (hasVoted) {
-		// 		$('form').hide();
-		// 	}
+			return electionInstance.voters(App.account); // here we access the voters mapping and read the current account to see if they exist in the mapping, if they exist they have already voted which in turn will return a boolean value which is true
+		}).then(function (hasVoted) {
+			// Do not allow a user to vote
+			if (hasVoted) {
+				$('form').hide(); // if the bool value is true we will hide the form
+			}
 			loader.hide();
 			content.show();
 		}).catch(function (error) {
@@ -107,19 +110,26 @@ App = {
 		});
 	},
 
-	// castVote: function () {
-	// 	var candidateId = $('#candidatesSelect').val();
-	// 	App.contracts.Election.deployed().then(function (instance) {
-	// 		return instance.vote(candidateId, { from: App.account });
-	// 	}).then(function (result) {
-	// 		// Wait for votes to update
-	// 		$("#content").hide();
-	// 		$("#loader").show();
-	// 	}).catch(function (err) {
-	// 		console.error(err);
-	// 	});
-	// }
-};
+	// in the below castVote() function,
+	// 1st we will ask for the candidateId which we will get to see from #candidatesSelect
+	// 2nd we will get instance of the deployed contract with the candidateId we got from select
+	// 3rd we will vote with App.account
+	// now we will wait till it processes, till then we should hide() the content and show() the loader
+	castVote: function() {
+		var candidateId = $('#candidatesSelect').val();
+		App.contracts.Election.deployed().then(function(instance) {
+		return instance.vote(candidateId);
+		}).then(function(_result) {
+		  // Wait for votes to update
+		  $("#content").hide();
+		  $("#loader").show();
+		}).catch(function(err) {
+		  console.error(err);
+		});
+	  }
+	};
+	
+
 
 $(function () {
 	$(window).load(function () {
